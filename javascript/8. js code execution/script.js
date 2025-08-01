2158e653294704a8b5a92915fef367ffb2bd8fb1
kanishk30 <kanishk71@gmail.com>
2025-08-01 08:36:02 +0530

    Non primtive example

--- javascript/8. js code execution/script.js
+++ javascript/8. js code execution/script.js
@@ -77,3 +77,86 @@ console.log("Array.isArray(arr) > ", Array.isArray(arr)); // true
 console.log("Array.isArray(hello) > ", Array.isArray("hello")); // false
 console.log("Array.isArray({}) > ", Array.isArray({})); // false
 
+
+
+// non primitive functions.
+
+function fun(param1){
+    console.log("Hello world, ", param1)
+}
+
+fun("Whatsup!"); // Hello world,  Whatsup!
+
+fun(); // Hello world,  undefined
+
+
+// Objects
+
+let obj2 = {
+    name: 'Jane',
+    age:20,
+    address: {
+        street: "Civil lines",
+        house: "C-121",
+        pin: 231001,
+
+    },
+    hobbies: ['cook', 'exercise'],
+    doesSmoke: false,
+    experience: {
+        tcs: {
+            yrs: 2,
+            role: "SDE"
+        },
+        infy: {
+            yrs: 1,
+            role: "Sr. SDE"
+        }
+    }
+}
+
+console.log(obj2.name)
+console.log(obj2['age']);
+
+console.log(obj2.address.street)
+console.log(obj2.hobbies)
+console.log(obj2.experience.tcs.role);
+
+// loops
+
+let arr2 = ['a', 'b', 'c'];
+
+// simple for loop
+for(let i = 0; i < arr2.length; i++){
+    console.log(arr2[i])
+}
+
+// for of loop - ARRAY
+
+console.log("for of loop starts ---")
+for( let char of arr2){
+    console.log(char)
+}
+
+// for in loop - OBJECT
+
+console.log('for in loop (objects) ---')
+
+for(let item in obj2 ){
+    console.log(item); // name
+    console.log(obj2[item]); // jane
+    console.log(obj2.item); // undefined
+}
+
+
+let cap = {
+    name:"Steve",
+    age: 34,
+    isAvenger: true,
+    }
+for(let key in cap){
+    console.log(key, " ", cap[key], " ", cap.key);
+    // name  Steve undefined
+    // age  34 undefined
+    // isAvenger  true undefined
+}
