267e83d7d3cc403f98c2c5bafa737f6a4e2ef90e
kanishk30 <kanishk71@gmail.com>
2025-08-04 08:17:13 +0530

    hoisting & EC

---
+++ 9. scope, oops/script.js
@@ -0,0 +1,16 @@
+let a = 20;
+
+if(true) {
+    let a = 10;
+     a = 5
+    console.log("inside if", a)
+}
+
+if(true){
+    var b = 20;
+    var b = 200;
+    console.log("var inside if", b)
+
+}
+
+console.log(a)
--- javascript/8. js code execution/code.js
+++ javascript/8. js code execution/code.js
@@ -1,12 +1,26 @@
-let a = 10;
-function fn(){
+// var a = 10;
+// function fn(){
     
-    console.log("i am fn");
-    function inner(){
-        console.log("i am inner function.")
-    }
+//     console.log("i am fn");
+//     function inner(){
+//         console.log("i am inner function.")
+//     }
     
-    inner();
-}
+//     inner();
+// }
 
-fn();
+// fn();
+
+console.log(x);     // undefined (var is hoisted)
+// console.log(y);     // ReferenceError: Cannot access 'y' before initialization
+console.log(foo()); // "Hello from foo"
+
+var x = 10;
+let y = 20;
+
+console.log(x); 
+console.log(y);  
+
+function foo() {
+    return "Hello from foo";
+}
