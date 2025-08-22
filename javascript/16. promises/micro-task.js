fb1a93113d0189d5b3a2c86be2734b3e376a3b20
kanishk30 <kanishk71@gmail.com>
2025-08-22 08:57:46 +0530

    Micro task

---
+++ javascript/16. promises/micro-task.js
@@ -0,0 +1,11 @@
+function logA() { console.log('A') }
+function logB() { console.log('B') }
+function logC() { console.log('C') }
+function logD() { console.log('D') }
+
+// Click the "RUN" button to learn how this works!
+logA();
+logB()
+setTimeout(logC, 0);
+logD();
+console.log("E")
