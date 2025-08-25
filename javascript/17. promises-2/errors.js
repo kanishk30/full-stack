1c987682de1e77c7b4514c7d07deff149924a2d4
kanishk30 <kanishk71@gmail.com>
2025-08-25 08:07:08 +0530

    finally case

--- javascript/17. promises-2/errors.js
+++ javascript/17. promises-2/errors.js
@@ -1,18 +1,31 @@
-// // catching errors in promise.
-// const fs = require("fs");
+// // // catching errors in promise.
+// // const fs = require("fs");
 
-// const rejectedPromise1 = Promise.reject("Explicit rejection");
-// rejectedPromise1.catch(e => console.log("Error1 caught: ", e))
+// // const rejectedPromise1 = Promise.reject("Explicit rejection");
+// // rejectedPromise1.catch(e => console.log("Error1 caught: ", e))
 
-// const fileRejPromise2 = fs.promises.readFile("./f111.txt");
-// fileRejPromise2.catch(e => console.log("Error1 caugh in file promise: ", e))
+// // const fileRejPromise2 = fs.promises.readFile("./f111.txt");
+// // fileRejPromise2.catch(e => console.log("Error1 caugh in file promise: ", e))
 
 
-// 3rd way. throw new Error in then
+// // 3rd way. throw new Error in then
+
+// Promise.resolve("Initial value")
+// .then(data => {
+//     console.log("Data: ", data);
+//     throw new Error("Error from then")
+// })
+// .catch(e => console.log("Cautht throw error in catch",  "Error class: ", e,  "just the message: ", e.message))
+
+// finally case
 
 Promise.resolve("Initial value")
-.then(data => {
-    console.log("Data: ", data);
-    throw new Error("Error from then")
+.finally((data) => {
+    console.log("data in finally", data)
+    return "Finally resolved"
 })
-.catch(e => console.log("Cautht throw error in catch",  "Error class: ", e,  "just the message: ", e.message))
+.then((result) => {
+    console.log("resolved in then: ", result)
+})
+// data in finally undefined
+// resolved in then:  Initial value
