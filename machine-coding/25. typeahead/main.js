d5c996678ec1166d504f8a5d224ac3f281183248
kanishk30 <kanishk71@gmail.com>
2025-09-15 08:43:35 +0530

    TYpeahead complete

--- machine-coding/25. typeahead/main.js
+++ machine-coding/25. typeahead/main.js
@@ -5,9 +5,26 @@ import fetchCountries from "./fetch-data.js";
 const inputBox = document.querySelector("#search-input");
 const suggestionBox = document.querySelector("#suggestion-box");
 
+function debounce(fn, delay = 500) {
+    let timerId;
+
+    return function (...args) {
+        if (timerId) {
+            console.log("clearTimeout called")
+            clearTimeout(timerId);
+        }
+        timerId = setTimeout(() => {
+            console.log("setTimeout called")
+
+            fn(...args);
+        }, delay);
+    };
+}
+
 
 const handleSearch = async (keyword) => {
     const countriesArr = await fetchCountries(keyword);
+    console.log("handleSearch called")
     const countriesNameArr = countriesArr.map(country => country.name.common);
     return countriesNameArr;
 }
@@ -20,6 +37,26 @@ const handleSuggestions = async (ev) => {
     createSuggestionBox(countriesNameArr);
 }
 
-inputBox.addEventListener("input", handleSuggestions)
+inputBox.addEventListener("input", debounce(handleSuggestions))
 
-const createSuggestionBox = () => {}
+const createSuggestionBox = (countriesNameArr) => {
+    // implement.
+    if(countriesNameArr.length) {
+        suggestionBox.classList.add("visible")
+    } else {
+        suggestionBox.classList.remove("visible")
+    }
+    // clear previous results.
+    suggestionBox.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+
+    for(let i = 0 ; i < countriesNameArr.length; i++){
+        const li = document.createElement("li");
+        li.textContent = countriesNameArr[i];
+        fragment.appendChild(li)
+    }
+
+    suggestionBox.appendChild(fragment)
+
+}
