b16feab72544ae20b961bb8a3b26c28628bd6411
kanishk30 <kanishk71@gmail.com>
2025-09-29 08:01:36 +0530

    React init

---
+++ react/my-first-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './Hello'
+
+
+function App() {
+
+  return (
+    <> 
+      <h1>Vite + React!!!!!!!</h1>
+      <Hello name="kanishk" />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+    </>
+  )
+}
+
+export default App
---
+++ react/my-first-react-app/src/Hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComponent = (props) => {
+    return <h2>Hello, I am a component. {props.name}</h2>
+}
+
+export default MyComponent;
