03c5716d46dcad868d2d02f9accf9c680e294e1e
kanishk30 <kanishk71@gmail.com>
2025-10-13 08:59:13 +0530

    Watchlist component

--- react/imdb/src/components/WatchList.jsx
+++ react/imdb/src/components/WatchList.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {BASE_IMG_URL, GENREIDS} from '../util'
 
 const WatchList = () => {
+      const [watchlist, setWatchlist] = useState([]);
+
+      useEffect(() => {
+          const moveisFromLocalStorage = localStorage.getItem("movies");
+          if(moveisFromLocalStorage){
+              setWatchlist(JSON.parse(moveisFromLocalStorage))
+          }
+      }, [])
+          
   return (
     <div>
-      I am watchlist component
+      <table className='w-full border border-gray-200 shadow-md m-5 rounded-lg'>
+          <thead>
+              <tr className='bg-gray-50'>
+                <th>Name</th>
+                <th>Ratings</th>
+                <th>Popularity</th>
+                <th>Genre</th>
+                <th>Delete</th>
+              </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-100 border-t border-gray-100'>
+            {watchlist.map((movieObj) => (
+              <tr>
+                <td className='flex items-center p-4'>
+                  <img src={BASE_IMG_URL+movieObj.backdrop_path} alt='Movie poster' className='h-[6rem] w-[10rem] p-4 object-fill' />
+                  <div className='font-medium text-gray-700 '>{movieObj.title}</div>
+                </td>
+                <td className='p-4'>{movieObj.vote_average}</td>
+                <td className='p-4'>{movieObj.popularity}</td>
+                {/* <td>{GENREIDS[movieObj.genre_ids[0]]}</td> */}
+                <td>
+                  {movieObj.genre_ids
+                    .map((genreId) => GENREIDS[genreId])
+                    .join(', ')
+                  }
+                </td>
+                <td className='p-4'><button>❌</button></td>
+              </tr>
+            ))}
+              
+          
+          </tbody>
+      </table>
     </div>
   )
 }
--- react/imdb/src/util.js
+++ react/imdb/src/util.js
@@ -1,1 +1,23 @@
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/original/";
+
+export const GENREIDS = {
+  "28": "Action",
+  "12": "Abenteuer",
+  "16": "Animation",
+  "35": "Komödie",
+  "80": "Krimi",
+  "99": "Dokumentarfilm",
+  "18": "Drama",
+  "10751": "Familie",
+  "14": "Fantasy",
+  "36": "Historie",
+  "27": "Horror",
+  "10402": "Musik",
+  "9648": "Mystery",
+  "10749": "Liebesfilm",
+  "878": "Science Fiction",
+  "10770": "TV-Film",
+  "53": "Thriller",
+  "10752": "Kriegsfilm",
+  "37": "Western"
+}
