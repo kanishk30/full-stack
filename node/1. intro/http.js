b729127a5f6c73f30cd9320fd2f0cabe7f3142fb
kanishk30 <kanishk71@gmail.com>
2025-11-14 08:05:38 +0530

    http module

---
+++ node/1. intro/http.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/plain")
+    res.write("Hello from http!")
+    res.end()
+})
+
+const server2 = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.write("<h1>Hello HTML</h1>")
+    res.end()
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log("Server is running on 3000....")
+})
+server2.listen(3001, 'localhost', (err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log("Server is running on 3000....")
+})
